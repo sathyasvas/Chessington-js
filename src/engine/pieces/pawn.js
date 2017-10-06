bb98ea776d0c92a1ef5fb61c6fcd77efbe3d880a
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    Fixed pawn movement when there is a blocking piece

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,17 +9,25 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+        let pawnAvailableMoves = new Array();
         if (this.player === Player.WHITE) {
-            if(location.row > 1) 
-                return new Array(Square.at(location.row + 1, location.col))
-            else
-                return new Array(Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col))       
+            if(board.getPiece(Square.at(location.row + 1, location.col)) == undefined) {
+                console.log("Inside GET White");
+                pawnAvailableMoves.push(Square.at(location.row + 1, location.col))
+                if(location.row == 1 && (board.getPiece(Square.at(location.row + 2, location.col)) == undefined))  
+                    pawnAvailableMoves.push(Square.at(location.row + 2, location.col))
+            }
         } else {
-            if(location.row < 6)
-                return new Array(Square.at(location.row - 1, location.col))
-            else
-                return new Array(Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col))
+            if(board.getPiece(Square.at(location.row - 1, location.col)) == undefined) {
+                console.log("Inside GET Black");
+                pawnAvailableMoves.push(Square.at(location.row - 1, location.col))
+            if(location.row == 6 && (board.getPiece(Square.at(location.row - 2, location.col)) == undefined)) {
+                console.log("Inside Black 2 spaces");
+                pawnAvailableMoves.push(Square.at(location.row - 2, location.col))
+            }
         }
-
+        console.log(pawnAvailableMoves);
+        }
+        return pawnAvailableMoves
     }
 }
