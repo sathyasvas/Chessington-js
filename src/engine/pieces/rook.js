7bcfff8b7bbfeda0dbe59d62d7afec046c13ef85
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-05-01 12:19:37 +0100

    Fixed the movement of rook

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
+import Player from '../player';
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,16 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let rookMoves= new Array();
+        for (let i = 0; i < 8; i++ ){
+            if(i != location.row)
+                rookMoves.push(Square.at(i, location.col))
+        }
+        for (let i = 0; i < 8; i++) {
+            if(i != location.col)
+                rookMoves.push(Square.at(location.row , i))
+            }
+    return rookMoves;
     }
 }
