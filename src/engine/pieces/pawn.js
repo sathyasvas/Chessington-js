fa0f2359f3e6472df443e8fc6e54fe6b1c2a3243
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-05-01 11:38:14 +0100

    Fixed the movement of pawns

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,9 +10,16 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+            if(location.row > 1) 
+                return new Array(Square.at(location.row + 1, location.col))
+            else
+                return new Array(Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col))       
         } else {
-            return Square.at(location.row - 1, location.col)
+            if(location.row < 6)
+                return new Array(Square.at(location.row - 1, location.col))
+            else
+                return new Array(Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col))
         }
+
     }
 }
