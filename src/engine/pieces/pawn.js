4bf192fdc85e449aabca30bfc120408299a514b7
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-05-03 17:08:51 +0100

    Completed the pawn movement test cases

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -25,28 +25,29 @@ export default class Pawn extends Piece {
         if (this.player === Player.WHITE) {
             if(location.row < 7 && !board.getPiece(nextSpaceWhite)) {
                 pawnAvailableMoves.push(nextSpaceWhite)
+
                 if(location.row == 1 && !board.getPiece(next2spacesWhite))  
                     pawnAvailableMoves.push(next2spacesWhite)
+
                 if(board.getPiece(acrossLeftWhite) && this.isOpposingPieceValid(acrossLeftWhite, board, Player.WHITE))
-                {
                     pawnAvailableMoves.push(acrossLeftWhite)
-                }
+        
                 if(board.getPiece(acrossRightWhite) && this.isOpposingPieceValid(acrossRightWhite, board, Player.WHITE))
-                {
                     pawnAvailableMoves.push(acrossRightWhite)
-                }
             }
         } else {
             if(location.row > 0 && !board.getPiece(nextSpaceBlack)) {
                 pawnAvailableMoves.push(nextSpaceBlack)
-            if(location.row == 6 && !board.getPiece(next2spacesBlack))
+
+                if(location.row == 6 && !board.getPiece(next2spacesBlack))
                 pawnAvailableMoves.push(next2spacesBlack)
-            if(board.getPiece(acrossLeftBlack)  && this.isOpposingPieceValid(acrossLeftBlack, board, Player.BLACK))
+
+                if(board.getPiece(acrossLeftBlack)  && this.isOpposingPieceValid(acrossLeftBlack, board, Player.BLACK))
                 pawnAvailableMoves.push(acrossLeftBlack)
-            if(board.getPiece(acrossRightBlack) && this.isOpposingPieceValid(acrossRightBlack, board, Player.BLACK))
+
+                if(board.getPiece(acrossRightBlack) && this.isOpposingPieceValid(acrossRightBlack, board, Player.BLACK))
                 pawnAvailableMoves.push(acrossRightBlack)
-        }
-        console.log(pawnAvailableMoves);
+            }
         }
         return pawnAvailableMoves
     }
